4248f5c5f57014f0f459381af862a7554957c400
joje <joje.attrs@gmail.com>
2017-01-13 02:50:42 +0900

    refactoring

--- dist/sidenav.js
+++ dist/sidenav.js
@@ -71,104 +71,132 @@ return /******/ (function(modules) { // webpackBootstrap
 	  if( arg.length === 1 ) lasturl = action;
 	  
 	  return this.each(function() {
-	    var nav = sidenavigation(this).init();
+	    if( !this.__sidenavinit__ ) {
+	      this.__sidenavinit__ = true;
+	      init(this);
+	    }
 	    
 	    if( arg.length === 1 ) {
-	      nav.select(action);
+	      select(this, action);
 	    } else if( !action || action === 'init' ) {
-	      nav.select(lasturl);
-	    } else if( action === 'open' ) {
-	      nav.open(value);
-	    } else if( action === 'close' ) {
-	      nav.close(value);
+	      init(this);
+	      select(this, lasturl);
 	    } else if( action === 'select' ) {
-	      nav.select(value);
+	      select(this, value);
 	    } else if( action ) {
 	      console.warn('unsupported action: ' + action);
 	    }
 	  });
 	};
 	
-	var sidenavigation = module.exports = function(current) {
-	  return {
-	    init: function(url) {
-	      var self = this;
-	      $(current).find('.accordion-toggle').each(function() {
-	        if( this.__init__ ) return;
-	        this.__init__ = true;
-	        
-	        $(this).on('click', function() {
-	          if( $(this).hasClass('menu-open') ) self.close(this);
-	          else self.open(this);
-	          
-	          return false;
-	        });
-	      });
-	      
-	      return this;
-	    },
-	    open: function(target) {
-	      if( !$(target).hasClass('accordion-toggle') ) return console.warn('target has not "accordion-toggle" class', target);
-	      
-	      var self = this;
-	      $(current).find('.accordion-toggle').each(function() {
-	        if( this !== target && $(this).hasClass('menu-open') ) {
-	          self.close(this);
-	        }
-	      });
-	      
-	      $(target).next('ul').slideDown('fast', 'swing', function() {
-	        $(target).addClass('menu-open');
-	      });
-	      
-	      return this;
-	    },
-	    close: function(target) {
-	      $(target).next('ul').slideUp('fast', 'swing', function() {
-	         $(target).removeClass('menu-open');
-	      });
-	      
-	      return this;
-	    },
-	    select: function(url) {
-	      if( !url ) return;
-	      
-	      var tobeselected, targetli, length, coselect, colen;
-	      $(current).find('.sidenav a[href]').each(function() {
-	        //if( tobeselected ) return;
-	        var href = this.getAttribute('href');
-	        if( !href ) return;
-	        
-	        if( href.indexOf(url) === 0 && (!length || length >= href.length) ) {
-	          length = href.length;
-	          tobeselected = this;
-	        }
-	        
-	        if( href && url.indexOf(href) === 0 && (!colen || colen < href.length) ) {
-	          colen = href.length;
-	          coselect = this;
-	        }
-	      });
-	      
-	      if( !tobeselected && coselect ) tobeselected = coselect;
-	      if( !tobeselected ) {
-	        return;
-	      }
-	      
-	      targetli = tobeselected.parentNode;
-	      
-	      $(current).find('.sidenav li').each(function() {
-	        if( this !== targetli ) $(this).removeClass('active');
-	      });
-	      $(targetli).addClass('active');
-	      
-	      var accordion = $(tobeselected).parent().parent().prev('.accordion-toggle').get(0);
-	      if( accordion ) this.open(accordion);
-	      
-	      return this;
-	    }
-	  };
+	function ontoggle() {
+	  toggle(this);
+	  return false;
 	}
+	
+	function onopen() {
+	  open(this);
+	  return false;
+	}
+	
+	function onclose() {
+	  close(this);
+	  return false;
+	}
+	
+	function init(root) {
+	  $(root).find('.accordion-toggle').each(function() {
+	    $(this).off('mousedown', ontoggle);
+	    $(this).on('mousedown', ontoggle);
+	  });
+	  
+	  $(root).find('.accordion-open').each(function() {
+	    $(this).off('mousedown', onopen);
+	    $(this).on('mousedown', onopen);
+	  });
+	  
+	  $(root).find('.accordion-close').each(function() {
+	    $(this).off('mousedown', onclose);
+	    $(this).on('mousedown', onclose);
+	  });
+	}
+	
+	function toggle(target) {
+	  var li = $(target);
+	  if( target.tagName.toLowerCase() !== 'li' ) li = $(target).closest('li');
+	  
+	  if( li.hasClass('accordion-opened') ) close(target);
+	  else open(target);
+	}
+	
+	function open(target) {
+	  var li = $(target);
+	  if( target.tagName.toLowerCase() !== 'li' ) li = $(target).closest('li');
+	  
+	  li.children('ul').slideDown('fast', 'swing', function() {
+	    $(this).closest('li').addClass('accordion-opened').children('a').addClass('menu-open');
+	  });
+	}
+	
+	function close(target) {
+	  var li = $(target);
+	  if( target.tagName.toLowerCase() !== 'li' ) li = $(target).closest('li');
+	  
+	  li.children('ul').slideUp('fast', 'swing', function() {
+	    $(this).closest('li').removeClass('accordion-opened').children('a').removeClass('menu-open');
+	  });
+	}
+	
+	function closeAll(root) {
+	  $(root).find('li.accordion-opened').each(function() {
+	    close(this);
+	  });
+	}
+	
+	function select(root, url) {
+	  if( !url ) return;
+	  
+	  var tobeselected, targetli, length, coselect, colen;
+	  $(root).find('li > a[href]').each(function() {
+	    //if( tobeselected ) return;
+	    var href = this.getAttribute('href');
+	    if( !href ) return;
+	    
+	    if( href.indexOf(url) === 0 && (!length || length >= href.length) ) {
+	      length = href.length;
+	      tobeselected = this;
+	    }
+	    
+	    if( href && url.indexOf(href) === 0 && (!colen || colen < href.length) ) {
+	      colen = href.length;
+	      coselect = this;
+	    }
+	  });
+	  
+	  if( !tobeselected && coselect ) tobeselected = coselect;
+	  if( !tobeselected ) {
+	    return;
+	  }
+	  
+	  targetli = tobeselected.parentNode;
+	  
+	  $(root).find('li').each(function() {
+	    if( this !== targetli ) $(this).removeClass('active').parents('li').removeClass('active');
+	  });
+	  
+	  $(targetli).addClass('active').parents('li').addClass('active')
+	  
+	  open(targetli.parentNode);
+	}
+	
+	module.exports = {
+	  init: init,
+	  select: select,
+	  toggle: toggle,
+	  open: open,
+	  close: close,
+	  closeAll: closeAll
+	};
 	
 
 
